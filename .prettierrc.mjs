8cc26b2464dfc43f4f6569cf5c3474ba227934d7
Tommy Carter <tommyjr@gmail.com>
2026-06-22 12:55:02 -0500

    Update prettier config

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,16 +1,8 @@
-'use strict';
-
-module.exports = {
+export default {
   plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
       files: '*.{js,gjs,ts,gts,mjs,mts,cjs,cts}',
-      options: {
-        singleQuote: true,
-      },
-    },
-    {
-      files: '*.{gjs,gts}',
       options: {
         singleQuote: true,
         templateSingleQuote: false,
