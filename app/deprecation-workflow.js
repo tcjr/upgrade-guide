347a4e2ab29d79246df8677e4ac35574056ac450
Tom Carter <tommyjr@gmail.com>
2026-06-26 14:57:41 -0500

    v6.7.2...v6.8.3

--- app/deprecation-workflow.js
+++ app/deprecation-workflow.js
@@ -1,8 +1,5 @@
 import setupDeprecationWorkflow from 'ember-cli-deprecation-workflow';
 
-/**
- * Docs: https://github.com/ember-cli/ember-cli-deprecation-workflow
- */
 setupDeprecationWorkflow({
   /**
     false by default, but if a developer / team wants to be more aggressive about being proactive with
@@ -10,15 +7,41 @@ setupDeprecationWorkflow({
   */
   throwOnUnhandled: false,
   workflow: [
-    /* ... handlers ... */
-    /* to generate this list, run your app for a while (or run the test suite),
-     * and then run in the browser console:
-     *
-     *    deprecationWorkflow.flushDeprecations()
-     *
-     * And copy the handlers here
-     */
-    /* example: */
-    /* { handler: 'silence', matchId: 'template-action' }, */
+    {
+      handler: 'log',
+      matchId: 'deprecate-import-test-from-ember',
+    },
+    {
+      handler: 'log',
+      matchId: 'warp-drive.deprecate-tracking-package',
+    },
+    {
+      handler: 'log',
+      matchId: 'deprecate-import--set-classic-decorator-from-ember',
+    },
+    {
+      handler: 'log',
+      matchId: 'importing-inject-from-ember-service',
+    },
+    {
+      handler: 'log',
+      matchId: 'deprecate-import--is-destroying-from-ember',
+    },
+    {
+      handler: 'log',
+      matchId: 'deprecate-import--is-destroyed-from-ember',
+    },
+    {
+      handler: 'log',
+      matchId: 'deprecate-import-destroy-from-ember',
+    },
+    {
+      handler: 'log',
+      matchId: 'deprecate-import--register-destructor-from-ember',
+    },
+    {
+      handler: 'log',
+      matchId: 'ember-data:deprecate-legacy-imports',
+    },
   ],
 });
--- testem.js
+++ testem.js
@@ -1,7 +1,7 @@
 'use strict';
 if (typeof module !== 'undefined') {
   module.exports = {
-    test_page: 'tests/index.html?hidepassed&nolint',
+    test_page: 'tests/index.html?hidepassed',
     disable_watching: true,
     launch_in_ci: ['Chrome'],
     launch_in_dev: ['Chrome'],
